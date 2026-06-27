c82d536ff0b6052a20a3a203dd1f403e3dd3af10
Martín Antúnez <maartin.ac@gmail.com>
2026-06-27 12:35:30 -0300

    Implementación del setup e instalación de la aplicación

--- Modulos/Application/main.tf
+++ Modulos/Application/main.tf
@@ -12,6 +12,44 @@ resource "aws_launch_template" "app" {
   tags = {
     Name = "Chewbacca-App"
   }
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+
+    # Actualizar los paquetes del sistema
+    yum update -y
+
+    # Habilitar el repositorio EPEL
+    amazon-linux-extras enable epel
+
+    # Instalar el paquete epel-release
+    yum install -y epel-release
+
+    # Agregar el repositorio REMI
+    yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+
+    # Habilitar PHP 5.4 desde REMI
+    yum-config-manager --enable remi-php54
+
+    # Instalar PHP y sus extensiones
+    yum install php php-cli php-common php-mbstring php-xml php-mysql php-fpm
+
+    # Instalar Apache y Git
+    yum install -y httpd git
+
+    # Habilitar Apache al iniciar la instancia
+    systemctl enable httpd
+
+    # Iniciar Apache
+    systemctl start httpd
+
+    # Clonar la aplicación
+    git clone https://github.com/ORT-FI-7417-SolucionesCloud/php-ecommerce-obligatorio.git
+
+    # Copiar la aplicación al DocumentRoot de Apache
+    cp -r php-ecommerce-obligatorio/* /var/www/html/
+
+EOF
+  )
 }
 
 resource "aws_autoscaling_group" "app" {
