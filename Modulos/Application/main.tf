2c3274d1149bebe5987df1ac1b0de422ecd2d51f
Martín Antúnez <maartin.ac@gmail.com>
2026-06-29 20:17:59 -0300

    Cambios en el user_data por el cambio de nombre del repositorio.

--- Modulos/Application/main.tf
+++ Modulos/Application/main.tf
@@ -46,10 +46,10 @@ resource "aws_launch_template" "app" {
     systemctl start httpd
 
     # Clonar la aplicación
-    git clone https://github.com/Pantufka/Chewbacca.git
+    git clone https://github.com/Pantufka/Obligatorio-ISC-2026.git
 
     # Copiar la aplicación al DocumentRoot de Apache
-    cp -r Chewbacca/php-ecommerce-obligatorio-main/* /var/www/html/
+    cp -r Obligatorio-ISC-2026/php-ecommerce-obligatorio-main/* /var/www/html/
 
     # Crear el archivo de configuración de la aplicación
     cat > /var/www/html/config.php <<EOL
